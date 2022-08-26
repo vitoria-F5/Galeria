07710d65e54bb7c8b5be34b6d332efb1de1282a7
20201tiimi0195 <vitoriaisabel77351@gmail.com>
2022-08-26 09:17:30 -0300

    Initial commit

---
+++ app/src/main/java/isabel/vitoria/galeria/MainActivity.java
@@ -0,0 +1,14 @@
+package isabel.vitoria.galeria;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
