5fb9acd27f7fcffbc74acea268ca1d4c4542cbfc
20201tiimi0195 <vitoriaisabel77351@gmail.com>
2022-08-26 10:01:23 -0300

    just

---
+++ app/src/main/java/isabel/vitoria/galeria/PhotoActivity.java
@@ -0,0 +1,14 @@
+package isabel.vitoria.galeria;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PhotoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_photo);
+    }
+}
